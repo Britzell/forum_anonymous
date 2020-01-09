90b9f7178901a11b177281f451e957217c6d96b3
Ines Roy <ines@toutenpixel.fr>
2020-01-09 12:06:05 +0100

    menu responsive

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="css/style.css">
+    <link href="css/navigation.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rajdhani:400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
--- inc/navigation.php
+++ inc/navigation.php
@@ -1,14 +1,29 @@
-<nav>
-  <div class="logo">
-      <a href="home.php"><img src="img/logo.png" alt=""></a>
-  </div>
-  <div class="naviga">
-    <ul>
-      <li><a href="topic?id=1">Forum</a></li>
-      <li><a href="createTopic">Créer un nouveau sujet de discution</a></li>
-      <li><a href="account">Mon profil</a></li>
-      <li><a href="settings">Paramètres</a></li>
-      <li><a href="logout"><i class="fas fa-times-circle"></i></a></li>
-    </ul>
-  </div>
-</nav>
+<header>
+    <i class="fas fa-bars ouvre" onclick="ouvreMenu()"></i>
+    <div class="logo">
+      <img src="img/logo.png" alt="">
+    </div>
+    <nav>
+      <i class="fas fa-times ferme" onclick="fermeMenu()"></i>
+      <ul>
+        <li><a href="topic?id=1">Forum</a></li>
+        <li><a href="createTopic">Créer un nouveau sujet de discution</a></li>
+        <li><a href="account">Mon profil</a></li>
+        <li><a href="settings">Paramètres</a></li>
+        <li><a href="logout"><i class="fas fa-times-circle"></i></a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+    <script>
+      function ouvreMenu(){
+        $("nav").addClass("menupresent")
+      }
+      function fermeMenu(){
+        $("nav").removeClass("menupresent")
+      }
+    </script>
