92a01e8043bbb5a23658bb4ecce89fbf7667b174
Alfred Br <alfred.brisse@gmail.com>
2020-01-09 12:04:06 +0100

    Update createCategory, list

--- createCategory.php
+++ createCategory.php
@@ -10,26 +10,37 @@
   }
 
   $category = getCategory($pdo);
+
+  require 'inc/header.php';
+    require 'inc/navigation.php';
 ?>
 
+<div class="contentSet">
 
-<form action="" method="post">
-  <div class="form-group">
-    <label for="name">Nom de la catégorie :</label>
-    <input type="text" name="name" required>
-  </div>
-  <div class="form-group">
-    <label for="category">Sous catégorie :</label>
-    <select class="" name="subcategory" required>
-      <option value="0">Cette catégorie n'est pas une sous-catégorie</option>
-      <?php foreach ($category as $c): ?>
-        <option value="<?= $c['id_category'] ?>" <?= $c['id_category'] == "z" ? "disabled" : "" ?>><?= $c['name'] ?></option>
-      <?php endforeach; ?>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="description">Description :</label>
-    <textarea name="description" cols="70" rows="5" required></textarea>
-  </div>
-  <button type="submit" name="button">Créer</button>
-</form>
+<i class="fas fa-comments iconSet"></i> <br>
+<h4>Nouvelle catégorie</h4>
+
+</div>
+
+<div class="formSetTopic">
+  <form class="settings" action="" method="post">
+    <div class="form-group">
+      <label for="name">Nom de la catégorie :</label>
+      <input type="text" name="name" required>
+    </div>
+    <div class="form-group">
+      <label for="category">Sous catégorie :</label>
+      <select class="" name="subcategory" required>
+        <option value="0">Cette catégorie n'est pas une sous-catégorie</option>
+        <?php foreach ($category as $c): ?>
+          <option value="<?= $c['id_category'] ?>" <?= $c['id_category'] == "z" ? "disabled" : "" ?>><?= $c['name'] ?></option>
+        <?php endforeach; ?>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="description">Description :</label>
+      <textarea name="description" cols="70" rows="5" required></textarea>
+    </div>
+    <button type="submit" name="button">Créer</button>
+  </form>
+</div>
--- inc/navigation.php
+++ inc/navigation.php
@@ -4,8 +4,11 @@
   </div>
   <div class="naviga">
     <ul>
-      <li><a href="topic?id=1">Forum</a></li>
+      <li><a href="list">Forum</a></li>
       <li><a href="createTopic">Créer un nouveau sujet de discution</a></li>
+      <?php if (!empty($_SESSION['user']['id_role']) && $_SESSION['user']['id_role'] > 1): ?>
+        <li><a href="createCategory">Créer une categorie</a></li>
+      <?php endif; ?>
       <li><a href="account">Mon profil</a></li>
       <li><a href="settings">Paramètres</a></li>
       <li><a href="logout"><i class="fas fa-times-circle"></i></a></li>
