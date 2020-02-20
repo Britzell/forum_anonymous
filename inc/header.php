04b3eedd74e04783b139c1d5ed2143cf476054ea
Alfred Br <alfred.brisse@gmail.com>
2020-02-20 10:03:50 +0100

    Add refresh css cache

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?= time() ?>">
     <link href="css/navigation.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Rajdhani:400,700&display=swap" rel="stylesheet">
